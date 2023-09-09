2e006d6dec9b2f73e299230acef8f0d624c2f09b
Niles Alexis <pl232602@ahschool.com>
2023-09-08 21:40:36 -0400

    Auto-upload

--- translate.sh
+++ translate.sh
@@ -1,7 +1,7 @@
 # /usr/bin/bash
-java -jar client-0.0.5.jar convert --output-format=csv environmental.db /data/environmental-dir
-java -jar client-0.0.5.jar convert --output-format=csv standard.db /data/standard-dir
-java -jar client-0.0.5.jar convert --output-format=csv particle_count.db /data/particle_count-dir
+java -jar client-0.0.5.jar convert --output-format=csv environmental.db data/environmental-dir
+java -jar client-0.0.5.jar convert --output-format=csv standard.db data/standard-dir
+java -jar client-0.0.5.jar convert --output-format=csv particle_count.db data/particle_count-dir
 rm environmental.db
 rm particle_count.db
 rm standard.db
