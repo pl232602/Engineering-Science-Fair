0bebe1641a8ec21600cb25191de62cef265409de
Niles Alexis <pl232602@ahschool.com>
2023-09-16 15:44:58 -0400

    Auto-upload

--- translate.sh
+++ translate.sh
@@ -3,7 +3,3 @@ sqlite3 -header -csv environmental.db "select * from data;" > data/environmental
 sqlite3 -header -csv particle_count.db "select * from data;" > data/particle_count.csv
 sqlite3 -header -csv standard.db "select * from data;" > data/standard.csv
 
-rm environmental.db
-rm particle_count.db
-rm standard.db
-
